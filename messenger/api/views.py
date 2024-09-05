b2ba79d0b06b28ed0239ea639ac3e7202bf5a9f1
Stasyaosan <smavrin38@gmail.com>
2024-09-05 20:56:38 +0300

    Начал делать api для приложения на kivy

---
+++ messenger/api/views.py
@@ -0,0 +1,71 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from django.views.decorators.csrf import csrf_exempt
+from meow_messenger.models import *
+import json
+
+
+@csrf_exempt
+def get_messages(request, id_chat, count):
+    if request.method == 'POST':
+        try:
+            messages = Message.objects.filter(chat=Group_chat.objects.filter(id=id_chat).first())
+        except:
+            return HttpResponse('Error data')
+        d = []
+        for m in messages[:count + 1]:
+            if m.file != '':
+                d.append({'file': f'/media/{str(m.file)}'})
+            d.append({
+                'id': m.id,
+                'text': m.text,
+                'user': m.user.id,
+                'chat': m.chat.id,
+                'file': '',
+                'datetime': str(m.datetime)
+            })
+        return HttpResponse(json.dumps(d))
+
+
+@csrf_exempt
+def get_chats(request):
+    if request.method == 'POST':
+        id_user = request.POST['id_user']
+        user = get_object_or_404(User, id=id_user)
+        chats = Group_chat.objects.filter(users__in=[user])
+        chats_new = []
+        for chat in chats:
+            members = []
+            for member in chat.users.all():
+                members.append({
+                    'id': member.id,
+                    'login': member.login,
+                    'avatar': f'/media/{str(member.avatar)}'
+                })
+            chats_new.append({
+                'id': chat.id,
+                'title': chat.title,
+                'description': chat.description,
+                'avatar': f'{str(chat.avatar)}',
+                'users': members
+            })
+        return HttpResponse(json.dumps(chats_new))
+    else:
+        return HttpResponse('Данные охраняются саблизубыми котиками^^')
+
+
+@csrf_exempt
+def add_chat(request):
+    if request.method == 'POST':
+        id_creator = int(request.POST['id_creator'])
+        title = request.POST['title']
+        description = request.POST['description']
+        creator = get_object_or_404(User, id=id_creator)
+
+        chat = Group_chat(title=title, description=description, creator=creator)
+        chat.save()
+        chat.users.add(creator)
+        return HttpResponse('Ok')
+    else:
+        return HttpResponse('Данные охраняются саблизубыми котиками^^')
--- messenger/messenger/urls.py
+++ messenger/messenger/urls.py
@@ -5,7 +5,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('meow_messenger.urls'))
+    path('', include('meow_messenger.urls')),
+    path('api/', include('api.urls'))
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
